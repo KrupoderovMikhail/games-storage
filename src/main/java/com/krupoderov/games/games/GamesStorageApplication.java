ff3ab18450c7e0218b3cfd6a8d4832f704943036
KrupoderovMikhail <krupoderov.mikhail@gmail.com>
2022-02-14 18:00:47 +0500

    Initial commit

---
+++ src/main/java/com/krupoderov/games/games/GamesStorageApplication.java
@@ -0,0 +1,13 @@
+package com.krupoderov.games.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krupoderov/games/games/GamesStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krupoderov.games.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
